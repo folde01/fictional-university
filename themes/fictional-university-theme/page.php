0d933a65226f487ccb7ae998f650a778eb4a4ec7
Jesse <folde01@users.noreply.github.com>
2020-01-02 13:59:14 +0000

    17. Menu of Child Page Links

--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -17,19 +17,46 @@ while (have_posts()) {
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
+    <?php
+    $theParent = wp_get_post_parent_id(get_the_id());
+    if ($theParent) { ?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+      </div>
+    <?php }; ?>
 
-    <!--
+
+    <?php 
+    // draw child links box only if page is a child or a parent
+
+    $isParent = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
+
+    if ($theParent or $isParent) { ?>
+
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php 
+        if ($theParent) {
+          // I'm a child so find children of my parent
+          $findChildrenOf = wp_get_post_parent_id(get_the_id());
+        } else {
+          // I'm a parent so find children of myself
+          $findChildrenOf = get_the_id();
+        }
+
+        wp_list_pages(array(
+          'title_li' => NULL,
+          'child_of' => $findChildrenOf,
+          'sort_column' => 'menu_order'
+        )); ?>
       </ul>
     </div>
-    -->
+    
+    <?php } ?>
+
 
     <div class="generic-content">
       <?php the_content(); ?>
